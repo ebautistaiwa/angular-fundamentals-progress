c2127d32ce213cf2c86f4e77f6ababdad3e1d483
Emanuel <ebautista@iwa.com.mx>
2017-11-02 15:02:12 -0600

    import y export

--- main.ts
+++ main.ts
@@ -1,51 +1,5 @@
-function ShoppingList(){
-    this.groceries = [];
-}
+import { uppercase } from './formatter'
 
-ShoppingList.prototype.addItem = function(item){
-    this.groceries = this.groceries.concat([item]);
-}
+let myName = 'Todd';
 
-ShoppingList.prototype.removeItem = function(item){
-    this.groceries = this.groceries.filter(function(grocery){
-        return item !== grocery;
-    });
-}
-
-var mylist = new ShoppingList();
-
-mylist.addItem('Banana');
-mylist.addItem('Apple');
-
-console.log(mylist.groceries);
-
-mylist.removeItem('Banana');
-
-console.log(mylist.groceries);
-
-class ShoppingList2{
-    groceries: string[];
-    constructor(){
-        this.groceries = [];
-    }
-
-    addItem(item){
-        this.groceries = [...this.groceries, item]
-    }
-
-    removeItem(item){
-        //shortest form
-        this.groceries = this.groceries.filter((grocery) => item !== grocery);
-        //longest form
-        // this.groceries = this.groceries.filter(function(grocery){
-        //     return item !== grocery;
-        // });
-    }
-}
-
-const myNewList = new ShoppingList2();
-
-myNewList.addItem('Pear');
-myNewList.addItem('Pizza');
-
-console.log(myNewList);
+console.log(uppercase(myName))
