f50f622730be8c080b0b1d7a620be2a08ff05750
Emanuel <ebautista@iwa.com.mx>
2017-11-02 16:27:41 -0600

    Enlazar eventos

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,18 +5,25 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <h1 [innerHTML]="title"></h1>
-      <h1>{{ title }}</h1>
-      <img [src]="logo">
-      <input type="text" [value]="name">
+      <button (click)="handleClick()"></button>
+      <input 
+        type="text" 
+        [value]="name"
+        (input)="handleInput($event)"
+        (blur)="handleBlur($event)">
+      <div>{{ name }}</div>
     </div>
   `
 })
 export class AppComponent{
-  title: string;
   name: string = "Todd";
-  logo: string = 'img/deskflip.jpg'
-  constructor(){
-    this.title = 'Ultimate Angular'
+  handleClick(){
+    this.name = 'Motto';
+  }
+  handleInput(event: any){
+    this.name = event.target.value;
+  }
+  handleBlur(event: any){
+    this.name = event.target.value;
   }
 }
