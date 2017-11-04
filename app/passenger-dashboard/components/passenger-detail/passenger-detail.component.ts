5f8fd2564f8ecd4c75a6286cf842e88bf970e843
Emanuel <ebautista@iwa.com.mx>
2017-11-03 18:55:32 -0600

    Agregar botones para editar

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -8,7 +8,12 @@ import { Passenger } from '../../models/passenger.interface'
     template: `
         <div>
             <span class="status" [class.checked-in]="detail.checkedIn"></span>
-            {{ detail.fullname }}
+            <div *ngIf="editing">
+                <input type="text" [value]="detail.fullname" (input)="onNameChange(name.value)" #name>
+            </div>
+            <div *ngIf="!editing">
+                {{ detail.fullname }}
+            </div>
             <p>{{ passenger | json }}
             <div class="date">
                 Check in date: 
@@ -17,11 +22,21 @@ import { Passenger } from '../../models/passenger.interface'
             <div class="children">
                 Children: {{ detail.children?.length || 0 }}
             </div>
+            <button (click)="toggleEdit()">{{ editing ? 'Done' : 'Edit' }}</button>
         </div>
     `
 })
 export class PassengerDetailComponent{
     @Input()
     detail: Passenger;
+    editing: boolean = false;
     constructor(){}
+
+    onNameChange(value: string){
+        this.detail.fullname = value;
+    }
+
+    toggleEdit(){
+        this.editing =  !this.editing;
+    }
 }
