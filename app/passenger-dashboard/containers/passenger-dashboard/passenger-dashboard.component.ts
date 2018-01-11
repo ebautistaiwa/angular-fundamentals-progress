b1a4e568db759993f28df425ee74e092e28fc5d9
Emanuel <ebautista@iwa.com.mx>
2018-01-11 17:58:37 -0600

    DataServices y dependency injection

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
 
-import { Passenger } from '../../models/passenger.interface'
+import { PassengerDashboardService } from '../../passenger-dashboard.service';
+
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
     selector: 'passenger-dashboard',
@@ -23,46 +25,9 @@ import { Passenger } from '../../models/passenger.interface'
 })
 export class PassengerDashboardComponent implements OnInit{
     passengers: Passenger[];
-    constructor(){}
+    constructor(private passengerService: PassengerDashboardService) {}
     ngOnInit(){
-        console.log("ngOnInit");
-        this.passengers = [
-            {
-              id: 1,
-              fullname: 'Stephen',
-              checkedIn: true,
-              checkInDate: 1490742000000,
-              children: null
-            },
-            {
-              id: 2,
-              fullname: 'Rose',
-              checkedIn: false,
-              checkInDate: null,
-              children: [{ name: 'Ted', age: 12 }, { name:'Chloe', age: 7 }]
-            },
-            {
-              id: 3,
-              fullname: 'James',
-              checkedIn: true,
-              checkInDate: 1491606000000,
-              children: null
-            },
-            {
-              id: 4,
-              fullname: 'Louise',
-              checkedIn: true,
-              checkInDate: 1488412800000,
-              children: [{ name: 'Jessica', age: 1 }]
-            },
-            {
-              id: 5,
-              fullname: 'Tina',
-              checkedIn: false,
-              checkInDate: null,
-              children: null
-            }
-          ];
+        this.passengers = this.passengerService.getPassengers();
     }
 
     handleEdit(event: Passenger){
