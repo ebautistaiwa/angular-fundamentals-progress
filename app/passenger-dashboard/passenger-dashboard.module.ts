73c0f5df756301ddf7dd9c856f94ff5d270b4e97
ebautista <ebautista@iwa.com.mx>
2018-09-28 14:17:53 -0500

    Form container component

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 
+//containers
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
+
+//components
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
@@ -12,6 +16,7 @@ import { PassengerDashboardService } from './passenger-dashboard.service'
 @NgModule({
     declarations: [
         PassengerDashboardComponent,
+        PassengerViewerComponent, 
         PassengerCountComponent,
         PassengerDetailComponent
     ],
@@ -20,7 +25,8 @@ import { PassengerDashboardService } from './passenger-dashboard.service'
         HttpModule
     ],
     exports: [
-        PassengerDashboardComponent
+        PassengerDashboardComponent, 
+        PassengerViewerComponent
     ],
     providers: [
         PassengerDashboardService
--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -25,6 +25,17 @@ export class PassengerDashboardService {
             );
     }
 
+    getPassenger(id: number): Observable<Passenger> {
+        return this.http
+            .get(`${PASSENGER_API}/${id}`)
+            .map((response: Response) => {
+                return response.json();
+            })
+            .catch((error: any) => 
+                Observable.throw(error.json())
+            );
+    }
+
     updatePassenger(passenger: Passenger): Observable<Passenger> {
         let headers = new Headers({
             'Contenty-Type': 'application/json'
